337a90e29bce4300600e6a8bea0b1d8f4e8dc3cb
Tadas Majeris <tadas.majeris@gmail.com>
2016-11-23 20:33:21 +0000

    User redirection test passing

--- features/user-login-routes.test.js
+++ features/user-login-routes.test.js
@@ -1,13 +1,23 @@
 describe("User Login", function () {
+  
+  beforeEach(function(){
+    server.call('logout');
+    server.execute(function () {
+      Package['xolvio:cleaner'].resetDatabase();
+    });
+  });
+
   describe("New user", function () {
     it("gets redirected to new character form on sign up", function () {
       browser.url('http://localhost:3000');
       browser.click('#login-sign-in-link');
       browser.click("#signup-link");
-      browser.setValue('inputlogin-email','Pikachu@pika.com');
-      // browser.setValue('[type="password"]', 'pikapika');
-      // browser.click('#login-buttons-password');
-      // expect(browser.url([url]).to.equal('http://localhost:3000/character/new');
+      browser.setValue('input#login-email','Pikachu@pika.com');
+      browser.setValue('input#login-password', 'pikapika');
+      browser.click('div#login-buttons-password');
+      browser.waitForExist("form.newCharacterForm", 4000);
+      expect(browser.getUrl()).to.equal('http://localhost:3000/character/new');
     });
   });
+
 });
