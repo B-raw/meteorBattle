d84b8b712f1e0caf8c15bfacc7b36d6d45c3cad0
Chetan Sanghani <crsanghani@gmail.com>
2016-11-24 10:49:42 +0000

    test written for redirect if user already has a character

--- features/user-login-routes.test.js
+++ features/user-login-routes.test.js
@@ -1,7 +1,15 @@
-function signInHelper(email, password) {
+function signUpHelper(email, password) {
   browser.url('http://localhost:3000');
   browser.click('#login-sign-in-link');
   browser.click("#signup-link");
+  browser.setValue('input#login-email', email);
+  browser.setValue('input#login-password', password);
+  browser.click('div#login-buttons-password');
+}
+
+function signInHelper(email, password) {
+  browser.url('http://localhost:3000');
+  browser.click('#login-sign-in-link');
   browser.setValue('input#login-email', email);
   browser.setValue('input#login-password', password);
   browser.click('div#login-buttons-password');
@@ -18,7 +26,7 @@ describe("User Login", function () {
   });
   describe("New user", function () {
     it("gets redirected to new character form on sign up", function () {
-      signInHelper("Pikachu@pika.com", "pikapika");
+      signUpHelper("Pikachu@pika.com", "pikapika");
       browser.waitForExist("form.newCharacterForm", 4000);
       expect(browser.getUrl()).to.equal('http://localhost:3000/character/new');
     });
@@ -30,6 +38,16 @@ describe("User Login", function () {
       .waitForExist('div.billboard', 4000);
       expect(browser.getUrl()).to.equal('http://localhost:3000/');
     });
-  });
 
+    it("redirects to /character if user already has a character created", function () {
+      signUpHelper('Pikachu@pika.com', "pikapika");
+      browser.waitForExist("form.newCharacterForm", 4000);
+      browser.setValue( '[name="name"]', 'Pikachu' )
+             .submitForm( 'form.newCharacterForm' );
+      server.call('logout');
+      signInHelper('Pikachu@pika.com', "pikapika");
+      browser.waitForExist("div.character", 4000);
+      browser.url('http://localhost:3000/character')
+    });
+  });
 });
