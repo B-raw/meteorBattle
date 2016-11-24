e9ee7b592ee671550f79389cb0aed48b7c79640b
B-raw <michaelbarry1989@hotmail.com>
2016-11-24 21:24:57 +0000

    only see other users online

--- client/Lobby/Lobby.js
+++ client/Lobby/Lobby.js
@@ -1,8 +1,22 @@
+Meteor.subscribe('userStatus');
+
 Template.Lobby.helpers({
  'characters': function(){
    var currentUserId = Meteor.userId()
-   console.log(currentUserId)
    return Characters.find({ createdBy: { $ne: currentUserId }});
+ },
+ 'usersOnline': function() {
+   var currentUserId = Meteor.userId()
+   console.log(Meteor.users.find({ "status.online": true }).fetch())
+   return Meteor.users.find({ "status.online": true, _id: { $ne: currentUserId } }, )
  }
-
 });
+
+// Template.userPill.labelClass = function() {
+//   if (this.status.idle)
+//     return "label-warning"
+//   else if (this.status.online)
+//     return "label-success"
+//   else
+//     return "label-default"
+// };
