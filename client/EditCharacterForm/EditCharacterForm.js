88ba438df6dc492b55d3c5aa0daa069f45da7728
B-raw <michaelbarry1989@hotmail.com>
2016-11-26 23:31:05 +0000

    Add cancel button to edit charac

--- client/EditCharacterForm/EditCharacterForm.js
+++ client/EditCharacterForm/EditCharacterForm.js
@@ -2,6 +2,10 @@ Template.EditCharacterForm.events({
   'submit .editCharacterForm'(event){
     event.preventDefault();
     var name = event.target.name.value;
+    Session.set('editMode', !Session.get('editMode'));
     Meteor.call('editCharacter', name);
+  },
+  'click .cancelEditCharacter'() {
+    Session.set('editMode', false);
   }
 });
--- client/NewCharacterForm/NewCharacterForm.js
+++ client/NewCharacterForm/NewCharacterForm.js
@@ -2,6 +2,7 @@ Template.NewCharacterForm.events({
   'submit .newCharacterForm'(event){
     event.preventDefault();
     var name = event.target.name.value;
+    Session.set('editMode', false);
     Meteor.call('newCharacter', name);
   }
 });
