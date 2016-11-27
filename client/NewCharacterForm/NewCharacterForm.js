6fddaaa0644c4e8c7447371b65dd335667890e3a
B-raw <michaelbarry1989@hotmail.com>
2016-11-27 16:28:48 +0000

    Edit routes

--- client/NewCharacterForm/NewCharacterForm.js
+++ client/NewCharacterForm/NewCharacterForm.js
@@ -1,8 +1,10 @@
 Template.NewCharacterForm.events({
   'submit .newCharacterForm'(event){
+    console.log("hello")
     event.preventDefault();
     var name = event.target.name.value;
     Session.set('editMode', false);
     Meteor.call('newCharacter', name);
+    FlowRouter.go('lobby');
   }
 });
